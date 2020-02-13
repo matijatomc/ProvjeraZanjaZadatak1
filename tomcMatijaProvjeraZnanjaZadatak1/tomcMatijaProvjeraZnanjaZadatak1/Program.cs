c45364b42afcd22f4495640cfa01ecd81b20a91d
matijatomc <55134628+matijatomc@users.noreply.github.com>
2020-02-13 10:24:16 +0100

    Add files via upload

---
+++ tomcMatijaProvjeraZnanjaZadatak1/tomcMatijaProvjeraZnanjaZadatak1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaProvjeraZnanjaZadatak1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 7;
+            double c = 12.5;
+            double d = 16.7;
+            double rj = (a + b + c + d) / 4;
+            Console.WriteLine("Peosjek brojeva 10, 7, 12.5, 16.7 je: " +rj);
+            Console.ReadKey();
+        }
+    }
+}
